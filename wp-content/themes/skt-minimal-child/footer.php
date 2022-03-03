a9c4d0c5325edf7a28498351d3f19920b0fad3f8
Beagle <sarah.e.beagle@gmail.com>
2022-03-02 17:36:57 -0800

    Final touches and browser compatability

--- wp-content/themes/skt-minimal-child/footer.php
+++ wp-content/themes/skt-minimal-child/footer.php
@@ -13,28 +13,30 @@
     </div><!-- #main -->
 
     <footer id="colophon" class="site-footer clearfix" role="contentinfo">
-      <div class="site-info">
-        <nav class="footer-menu-navigation" role="navigation">
-          <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'footer-menu', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-        </nav>
+      <div class="footer-wrapper">
+        <div class="site-info">
+          <nav class="footer-menu-navigation" role="navigation">
+            <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'menu_class' => 'footer-menu', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+          </nav>
 
-        <div class="logo footer-logo">
-            <a href="<?php echo site_url('/home/') ?>"><?php skt_minimal_the_custom_logo(); ?></a>
-        </div> 
+          <div class="logo footer-logo">
+              <a href="<?php echo site_url('/home/') ?>"><?php skt_minimal_the_custom_logo(); ?></a>
+          </div> 
 
-        <div class="footer-contact">
-          <?php while ( have_posts()) : the_post(); 
-                    $email = get_field("main_email", 325);
-                    $newsubject = get_field("subject_text", 325);
-                ?>
-          <?php endwhile; ?>      
-          <p>CONTACT</p>
-          <p id="footer-phone">(310) 920-1502 </p>
-          <a href="mailto:<?php echo $email ?>?subject=<?php echo $newsubject ?>"><?php echo $email ?></a>
-           
-        </div>
-        
-      </div><!-- .site-info -->
+          <div class="footer-contact">
+            <?php while ( have_posts()) : the_post(); 
+                      $email = get_field("main_email", 325);
+                      $newsubject = get_field("subject_text", 325);
+                  ?>
+            <?php endwhile; ?>      
+            <p>CONTACT</p>
+            <p id="footer-phone">(310) 920-1502 </p>
+            <a href="mailto:<?php echo $email ?>?subject=<?php echo $newsubject ?>"><?php echo $email ?></a>
+             
+          </div>
+          
+        </div><!-- .site-info -->
+      </div>  
       <div id="copyright-area">
         <div class="maxwidth copyright-area-small">
           <p>Copyright</p>
